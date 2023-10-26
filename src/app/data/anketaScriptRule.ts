5ceee970c22972f7b89c3b7c5c5d6e55bbb0c933
Dmitry <Dmitryxdw@mail.ru>
2023-10-26 23:34:50 +0300

    home commit 26.10.2023

--- src/app/data/anketaScriptRule.ts
+++ src/app/data/anketaScriptRule.ts
@@ -10,30 +10,62 @@ export class AnketaScriptRule {
 
   getPrintRules(): PDFDocObject[] {
     let resRul = [...constRule]
-    let componentListId: number[] = [25, 184,27, 5, 7, 185]
+    let componentListId: number[] = [25,184,27,5,7,185, 17,22,70,214, 30,32,34,195,38,40,42,44] /**Это ИД элементов котрые нужно отобразить на печать*/
 
     componentListId.forEach(id => {
       let comp = this.getComponentFromDoc(id)
+
+      console.log("id:" + id)
+      console.log("comp:")
+      console.log(comp)
+
       if (comp) {
         if (comp.componentType === IceComponentType.AREA ||
             comp.componentType === IceComponentType.SELECT ||
+            comp.componentType === IceComponentType.PLACE ||
               (comp.componentType === IceComponentType.INPUT
                 && comp.inputType != "checkbox"
                 && comp.inputType != "radio")) {
           resRul.push(...this.setPdfRuleForAreaInputComponent(comp))
         }
-      }
+        if (comp.componentType === IceComponentType.INPUT && comp.inputType === "checkbox" && comp.visible)
+          resRul.push(...this.setPdfRuleForCheckBoxComponent(comp))
+        if (comp.componentType === IceComponentType.TEXT)
+          resRul.push(...this.settPdfRuleForTextComponent(comp))
+
+        }
     })
 
 
     return resRul as PDFDocObject[]
   }
 
-  setPdfRuleForAreaInputComponent(comp: ComponentMaket): PDFDocObject[]{
+  private settPdfRuleForTextComponent(comp: ComponentMaket): PDFDocObject[] {
+    let string = ""
+    switch (comp.componentID){
+      case 90 : string = "Сведения о присутствии или отсутствии по указанному в п. 6 адресу фактического местонахождения организации (в т.ч. и при совпадении его с адресом юридического лица), его постоянно действующих органов управления, иного органа или лица, которые имеют право действовать от имени организации без доверенности"
+    }
+
     return [{
       redLine: true,
       fontSize: 11,
-      value: comp.placeHolder + ":",
+      value: string + ":",
+      fontStyle: "bold",
+    }];
+  }
+
+  private setPdfRuleForCheckBoxComponent(comp: ComponentMaket): PDFDocObject[] {
+    console.log(comp)
+    return [{
+      value: comp.value ? "Да" : "Нет"
+    }];
+  }
+
+  setPdfRule(label: string, value: string): PDFDocObject[]{
+    return [{
+      redLine: true,
+      fontSize: 11,
+      value: label,
       fontStyle: "bold",
     },
       {
@@ -41,11 +73,21 @@ export class AnketaScriptRule {
         subLineHeight: 2
       },
       {
-        value: comp.value ? comp.value : "Не указано."
+        value: value
       },
       {
         type: "space"
       }]
+  }
+
+  setPdfRuleForAreaInputComponent(comp: ComponentMaket): PDFDocObject[]{
+    let valueString =  "Не указано."
+    if(comp.componentType === IceComponentType.PLACE)
+      valueString = (comp.value && comp.value.placeString) ? comp.value.placeString : valueString
+    else
+      valueString = comp.value ? comp.value : valueString
+
+    return this.setPdfRule((comp.placeHolder ? comp.placeHolder : "") + ":", valueString)
   }
 
   getComponentFromDoc(id: number): ComponentMaket {
